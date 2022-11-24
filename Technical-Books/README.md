bf64e886183525736e51a04c3dd4c05e8f45fe25
Moussa-Kalam <m.amzat@alustudent.com>
2022-11-25 01:00:13 +0200

    Extend a new functionality to the inventory application of bookstore

---
+++ Technical-Books/README.md
@@ -0,0 +1,17 @@
+# Technical Books
+
+## What You'll Do
+
+Expanding on your previous book application you now want to create a class for technical books. Using the Book class constructed in the last example, extend the functionality to the new TechnicalBook class.
+
+The new TechnicalBook class will take a book edition as a fifth argument.
+
+You should add a getEdition method which prints `The current version of this book is ${version}`.
+
+## Requirements
+
+- Use JavaScript's class notation
+
+## Instructions
+
+Develop your solution in the `technicalBooks.js` file. If this challenge requires you to interact with the DOM, use the `index.html` file to complete your DOM manipulation.
---
+++ Technical-Books/technicalBooks.js
@@ -0,0 +1,45 @@
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  // Getter
+  get availability() {
+    return this.getAvailability();
+  }
+
+  // Method
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock";
+    }
+    return "In stock";
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+class TechnicalBook extends Book {
+  constructor(title, author, ISBN, numCopies, edition) {
+    super(title, author, ISBN, numCopies);
+    this.edition = edition;
+  }
+  getEdition() {
+    return `The current version of this book is ${this.edition}`;
+  }
+}
+
+const newTechnicalBook = new TechnicalBook("How successful people think", "John C. Maxwell", "978-1-59995-168-3", 25, "2.0");
+console.log(newTechnicalBook.availability);
+console.log(newTechnicalBook.getEdition());
